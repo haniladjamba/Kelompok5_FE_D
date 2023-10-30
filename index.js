384b2ccf0510f459f31b31fdd49fa3f5dd3d14ff
haniladjamba <s22110313@student.unklab.ac.id>
2023-10-30 05:47:45 -0700

    patch 1.0

--- index.js
+++ index.js
@@ -14,3 +14,7 @@ console.log("Emang boleh se jerrico itu eheee")
 
 console.log("Tumewu, Benedict Jonathan")
 
+// --------------------------------------------------
+// MergeConf Patch 1
+console.log("Patch 1.0");
+
